caa0215059e61deab48c18f274cd86155b71275b
Igor Brusko <tailsmiles2015b@gmail.com>
2024-07-25 12:56:29 +0700

    Rebuild

--- Build/Builds.framework.js
+++ Build/Builds.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  6048608: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 6048669: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 6048733: function() {return Module.webglContextAttributes.powerPreference;},  
- 6048791: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 6048846: function($0) {performance.now = function() { return $0; };},  
- 6048894: function($0) {performance.now = function() { return $0; };},  
- 6048942: function() {performance.now = Module['emscripten_get_now_backup'];}
+  6048624: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 6048685: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 6048749: function() {return Module.webglContextAttributes.powerPreference;},  
+ 6048807: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 6048862: function($0) {performance.now = function() { return $0; };},  
+ 6048910: function($0) {performance.now = function() { return $0; };},  
+ 6048958: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
